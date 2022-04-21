21e8818c1283fd71674930c14e6207e2186bf2be
DuongVanTrung31 <trung.duong.dt31@gmail.com>
2022-04-21 09:38:32 +0700

    upload img

--- src/app/homepage/topdetail/topdetail.component.ts
+++ src/app/homepage/topdetail/topdetail.component.ts
@@ -1,5 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {User} from "../../models/user";
+import {UserService} from "../../services/user.service";
+import {finalize} from "rxjs";
+import {AngularFireStorage} from "@angular/fire/compat/storage";
 
 @Component({
   selector: 'app-topdetail',
@@ -7,10 +10,34 @@ import {User} from "../../models/user";
   styleUrls: ['./topdetail.component.css']
 })
 export class TopdetailComponent implements OnInit {
-  @Input() user!: User
-  id = parseInt(<string>localStorage.getItem("ID"))
-  constructor() { }
+  user!: User
+  id = parseInt(<string>localStorage.getItem("ID"));
+  @ViewChild('uploadFile', {static: true}) public imageDom: ElementRef | undefined
+  selectedImage: any;
+  constructor(private userService:UserService,
+              private storage: AngularFireStorage) { }
 
   ngOnInit(): void {
+    this.userService.getUserInfo(this.id).subscribe(data => this.user = data)
+  }
+
+  uploadFireBase() {
+    this.selectedImage = this.imageDom?.nativeElement.files[0];
+    if (this.selectedImage != null) {
+      const filePath = this.selectedImage.name;
+      const fileRef = this.storage.ref(filePath);
+      this.storage.upload(filePath, this.selectedImage).snapshotChanges()
+        .pipe(
+          finalize(
+            () => (fileRef.getDownloadURL()
+              .subscribe((url) => {
+                this.user.avatarUrl = url
+                this.userService.updateImg(this.id,{avatarUrl: url}).subscribe(() => {
+                  this.ngOnInit()
+                })
+              }))
+          )
+        ).subscribe()
+    }
   }
 }
